7d49fabb2eabd9f92782eccebbee64949841a416
Cobbiee <equoby@outlook.com>
2023-06-15 14:30:56 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+   * main - A program that prints a string using puts
+   *
+   * return: Always 0 (success)
+   */
+int main(void)
+	char str[] = "Programming is like building a multilingual puzzle";
+	puts(str);
+	return (0);
