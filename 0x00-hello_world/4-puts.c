2df026a519159e2427eba3d3ed29a31f64518207
Cobbiee <equoby@outlook.com>
2023-06-15 15:23:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-/**
- * main - A program that prints a string using puts
- *
- * return: Always 0 (success)
- */
+/*
+   * main - A program that prints a string using puts function
+   * return: always 0 (success)
+   */
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "Programming is like building a multilingual puzzle\n";
 	puts(str);
 	return (0);
 }
