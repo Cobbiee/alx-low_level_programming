8efa8cc0bb371d2321b3d41c3e0af7dfe632d057
Cobbiee <equoby@outlook.com>
2023-06-15 17:22:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints to the standard error.
+ * main - A program that prints to the stderr without using printf and puts.
  *
  * Return: Always 1 (Failure)
  */
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
-return (1);
+	return (1);
 }
