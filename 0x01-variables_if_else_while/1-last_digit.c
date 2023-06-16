fb41bcfe6d5c8b030d3c7f7e769cbb58b38835a2
Cobbiee <equoby@outlook.com>
2023-06-16 11:15:09 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,26 +10,26 @@
  */
 int main(void)
 {
-	int n, last_digit;
+	int n, l_d;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 /* enter your code here*/
 
-	last_digit = n % 10;
-	if (last_digit > 5)
+	l_d = n % 10;
+	if (l_d > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+	printf("Last digit of %d is %d and is greater than 5\n", n, l_d);
 	}
 
-	else if (last_digit < 6)
+	else if (l_d == 0)
 	{
-	printf("Last digit of %d is %d and is less than 6\n", n, last_digit);
+	printf("Last digit of %d is %d and is 0\n", n, l_d);
 	}
 
 	else
 	{
-	printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l_d);
 	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the alphabet in lower case.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
