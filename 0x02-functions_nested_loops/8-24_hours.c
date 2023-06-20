edb3d769542166b91e2960b20933e0034fc31f3d
Cobbiee <equoby@outlook.com>
2023-06-20 15:51:01 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,24 @@
-#include "main"
+#include "main.h"
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- *
- * staring from 00: 00 to 23:59
+ * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
-	int = i, j;
+	int i, j;
 
 	i = 0;
 
 	while (i < 24)
 	{
 		j = 0;
-
 		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
-			_puthcar((j / 10) + '0');
+			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
 			j++;
