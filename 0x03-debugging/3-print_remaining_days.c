0edb03a9418486a035d6c7fa492348677fac7c11
Cobbiee <equoby@outlook.com>
2023-06-21 08:55:14 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,35 +2,36 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many days are left
- * in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return: void
- */
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
+
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-    {
-        if (month > 2)
-        {
-            day++;
-        }
+	int days_in_year;
 
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
-    }
-    else
-    {
-        if (month == 2 && day == 29)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
-    }
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	{
+		/* leap year */
+		days_in_year = 366;
+		if (month > 2 && day >= 60)
+			day++;
+	}
+	else
+	{
+		/* not a leap year */
+		days_in_year = 365;
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			return;
+		}
+	}
+
+	printf("Day of the year: %d\n", day);
+	printf("Remaining days: %d\n", days_in_year - day);
 }
