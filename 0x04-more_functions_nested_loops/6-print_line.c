592466f231cbe9042138f21b5f42d28373d9a1c3
Cobbiee <equoby@outlook.com>
2023-06-22 10:55:22 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal.
+ * @n: THe number of times the the character should be printed
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = o; i < n; i++)
+		{
+			_putchar('_')
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -9,6 +8,6 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-
+void print_line(int n);
 
 #endif /* MAIN_H */
