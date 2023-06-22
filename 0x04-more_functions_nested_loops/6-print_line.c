a279e0c75f8ab7d78ded89c04d2484d0a0aa97af
Cobbiee <equoby@outlook.com>
2023-06-22 11:05:51 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,9 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	for (i = 0; i < n; i++)
 	{
-		_putchar('\n');
+		_putchar('_');
 	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
-	}
+	_putchar('\n');
 }
