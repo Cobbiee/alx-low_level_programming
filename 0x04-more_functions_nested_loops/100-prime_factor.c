e836a41977e88d2f7432eca4368e1b86efacff0d
Cobbiee <equoby@outlook.com>
2023-06-22 16:39:47 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - initializes the number.
+ *
+ * largest_prime_factor - Finds the larfest prime factor.
+ *
+ * @n: Checks the factor division.
+ *
+ * Return: Always 0.
+ */
+
+long largest_prime_factor(long n)
+{
+	long largestFactor = -1;
+	long i;
+
+    /* Divide by 2 until it's no longer divisible */
+	while (n % 2 == 0)
+	{
+		largestFactor = 2;
+		n /= 2;
+	}
+
+	/* Check odd factors up to the square root of n */
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			largestFactor = i;
+			n /= i;
+		}
+	}
+
+	/* If n is prime and greater than 2 */
+	if (n > 2)
+		largestFactor = n;
+	return (largestFactor);
+}
+
+int main(void)
+{
+	long number = 612852475143;
+	long largestFactor = largest_prime_factor(number);
+	printf("%ld\n", largestFactor);
+	return (0);
+}
