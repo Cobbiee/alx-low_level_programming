4cd5163af0b88a1110005f2b444c640a49a2b230
Cobbiee <equoby@outlook.com>
2023-06-22 08:06:33 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the umbers from 0 to 9, followed by a new line
+ *
+ * Return: void.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+
+		_putchar(i + '0');
+
+	_putchar('\n');
+}
