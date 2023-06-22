afb77e7557a178694d4d85c66f05bbd0658b9789
Cobbiee <equoby@outlook.com>
2023-06-22 15:22:45 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints numbers from 1 to 100
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", num);
+		}
+	}
+	printf("\n");
+	return (0);
+}
