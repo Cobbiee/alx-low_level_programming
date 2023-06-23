03c5b5aefb2d413ccdb1c99882bfcfc0ffa2979c
Cobbiee <equoby@outlook.com>
2023-06-23 06:47:37 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,28 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && num % 5 == 0)
+		if (num % 3 == 0 && num % 5 != 0)
 		{
-			printf("FizzBuzz");
+			printf(" Fizz");
 		}
-		else if (num % 3 == 0)
+		else if (num % 5 == 0 && num % 3 != 0)
 		{
-			printf("Fizz");
+			printf(" Buzz");
 		}
-		else if (num % 5 == 0)
+		else if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("Buzz");
+			printf(" FizzBuzz");
+		}
+		else if (num == 1)
+		{
+			printf(" %d", num);
 		}
 		else
 		{
-			printf("%d", num);
+			printf(" %d", num);
 		}
 	}
 	printf("\n");
+
 	return (0);
 }
