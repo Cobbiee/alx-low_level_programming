eed2b56c1c0ef4e56baa495e4766b8551416592d
Cobbiee <equoby@outlook.com>
2023-06-29 17:46:52 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,41 +1,24 @@
 #include "main.h"
 
 /**
- * is_separator - Checks if a character is aword separartor
- * @c: THe character to check
- *
- * Return: 1 if the character is a separator, 0 otherwise
- */
-int is_separator(char c)
-{
-	char sep[] = " \t\n,;.!?\"(){}";
-	int i;
-
-	for (i = 0; sep[i] != '\0'; i++)
-	{
-		if (sep[i] == c)
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * cap_string - Capitalizes all words of a string
- * @str: The string to capitalize
+ * *cap_string - Capitalizes all words of a string
+ * @str: string to capitalize
  *
  * Return: A pointer to the modified string
  */
-char cap_string(char *str)
-{
-	int j;
 
-	for (j = 0; str[j] != '\0'; j++)
+char *cap_string(char *str)
+{
+	int i, j;
+	char separators[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (j == 0 || is_separator(str[j - 1]))
-		{
-			if (str[j] >= 'a' && str[j] <= 'z')
-				str[j] = str[j] - 32;
-		}
+		if (str[0] >= 97 && str[0] <= 122)
+			str[0] = str[0] - 32;
+				for (j = 0; separators[j] != '\0'; j++)
+					if (str[i] == separators[j] && str[i + 1] >= 97 && str[i + 1] <= 122)
+						str[i + 1] = s[i + 1] - 32;
 	}
 	return (str);
 }
