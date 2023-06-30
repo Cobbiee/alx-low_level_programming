cae2a920bbba55346c6fd469b9ec16d1f2e16183
Cobbiee <equoby@outlook.com>
2023-06-29 17:51:13 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 
 /**
- * *cap_string - Capitalizes all words of a string
- * @str: string to capitalize
+ * *cap_string - this is awesome
+ * @s: pointer to char params
  *
- * Return: A pointer to the modified string
+ * Return: *s
  */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int i, j;
-	char separators[] = " \t\n,;.!?\"(){}";
+	char delimeters[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (str[0] >= 97 && str[0] <= 122)
-			str[0] = str[0] - 32;
-				for (j = 0; separators[j] != '\0'; j++)
-					if (str[i] == separators[j] && str[i + 1] >= 97 && str[i + 1] <= 122)
-						str[i + 1] = s[i + 1] - 32;
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] = s[0] - 32;
+				for (j = 0; delimeters[j] != '\0'; j++)
+					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
 	}
-	return (str);
+	return (s);
 }
