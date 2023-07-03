6cfdc8994c256c7560cfd3acdce1e536db702c63
Cobbiee <equoby@outlook.com>
2023-07-03 09:23:46 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - Fills memory with a constant byte
+ * @dest: Pointer to the memory area
+ * @src: The constant byte to fill with
+ * @n: Number of bytes to fill
+ *
+ * Return: Pointer to the memory area s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int = i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
