962065081e0f4fa86d4e32c1cd336e2e80ed825f
Cobbiee <equoby@outlook.com>
2023-07-03 12:44:40 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: Pointer to the string
+ * @c: Character to locate
+ *
+ * Return: Pointer to the first occurance of c in s, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+
+	return (NULL);
+}
