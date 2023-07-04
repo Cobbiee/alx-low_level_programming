7e2fb1e0909b553a1bc2d7567608b39ef1300237
Cobbiee <equoby@outlook.com>
2023-07-04 02:56:20 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,33 +1,26 @@
 #include "main.h"
 
 /**
- * inCharSet - checks if a character is in a set of characters
- * @c: character to be checked
- * @accept: string containing the characters to match
- * Return: 1 if c is in accept, 0 otherwise
+ * _strpbrk - Searches a string for any of a set of bytes
+ *
+ * @s: Character to check
+ *
+ * @accept: Character to look for
+ *
+ * Return: Character
  */
-int inCharSet(char c, char *accept)
-{
-	int i = 0;
 
-	while (accept[i])
-		if (c == accept[i++])
-			return (1);
-	return (0);
-}
-
-/**
- * _strpbrk - searches a string for any of a set of bytes
- * @s: string to be scanned
- * @accept: string containing the characters to match
- * Return: pointer to the byte in s that matches one of the bytes in accept,
- */
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0;
+	int i, k;
 
-	while (s[i])
-		if (inCharSet(s[i++], accept))
-			return (s + i - 1);
-	return (NULL);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (k = 0; accept[k] != '\0'; k++)
+		{
+			if (s[i] == accept[k])
+				return (s + i);
+		}
+	}
+	return (0);
 }
