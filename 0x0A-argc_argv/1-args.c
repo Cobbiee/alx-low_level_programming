55712fd6538581d6322c68e730095c267380552b
Cobbiee <equoby@outlook.com>
2023-07-07 10:57:01 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of comman-line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
