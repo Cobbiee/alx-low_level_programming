13b57d453ef0e1cf3cd0df7a6cf4ad8e63f11e59
Cobbiee <equoby@outlook.com>
2023-07-10 09:31:35 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,14 +14,21 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
+	{	
 		return (NULL);
+	}
 
 	array = malloc(size * sizeof(char));
+	
 	if (array == NULL)
+	{	
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
+	{	
 		array[i] = c;
+	}
 
 	return (array);
 }
