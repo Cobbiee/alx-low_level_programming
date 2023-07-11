bb8eb8b7fed8c0a8a23c52af620284f34dded75a
Cobbiee <equoby@outlook.com>
2023-07-11 00:51:34 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(width * sizeof(int))
+		grid[i] = malloc(width * sizeof(int));
 			if (grid[i] == NULL)
 			{
 				for (j = 0; j < i; j++)
