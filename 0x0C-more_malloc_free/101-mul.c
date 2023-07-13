9490adbb323c378d7b03c166a0c227b9e6b8fd26
Cobbiee <equoby@outlook.com>
2023-07-13 16:56:32 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,64 +1,94 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * multiply - Multiplies two positve numbers
- * @num1: First number
- * @num2: Second number
- *
- * Return: Result of the multiplication
+ * isNumber - checks if a string is a number
+ * @s: string to check
+ * Return: 1 if number, 0 if not
  */
-unsigned int multiply(unsigned int num1, unsigned int num2)
+int isNumber(char *s)
 {
-	return (num1 * num2);
-}
+	int i;
 
-/**
- * is_pos_num - Checks of a string is a positve number
- * @str: The string to check
- *
- * Return: 1 if the string is a positive number, 0 otherwise
- */
-int is_pos_num(char *str)
-{
-	while (*str)
-	{
-		if (*str < '0' || *str > '9')
+	for (i = 0; s[i]; i++)
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		str++;
-	}
-
 	return (1);
 }
 
 /**
- * main - Entry point of the program
- * @argc: Number of command-line arguments
- * @argv: Array pof command-line arguments
- *
- * Return: 0 on success, 1 on error
+ * print_result - prints the result of the multiplication
+ * @result: array of integers
+ * @len: length of result
+ * Return: void
  */
-int main(int argc, char *argv[])
+void print_result(int *result, int len)
 {
-	unsigned int num1, num2, result;
+	int i;
 
-	if (argc != 3)
+	/* remove leading zeros */
+	for (i = 0; i < len && result[i] == 0; i++)
+		;
+
+	/* if all digits are 0, print 0 */
+	if (i == len)
+		printf("0");
+
+	/* print the result */
+	for (; i < len; i++)
+		printf("%d", result[i]);
+
+	printf("\n");
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @ac: number of arguments
+ * @av: array of arguments
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	int i, j, num1_len, num2_len;
+	int *result;
+
+	if (ac != 3 || !isNumber(av[1]) || !isNumber(av[2]))
 	{
 		printf("Error\n");
-			return (1);
+		exit(98);
 	}
 
-	if (!is_pos_num(argv[1]) || !is_pos_num(argv[2]))
+	num1_len = strlen(av[1]);
+	num2_len = strlen(av[2]);
+
+	result = calloc(num1_len + num2_len, sizeof(int));
+	if (result == NULL)
 	{
 		printf("Error\n");
-		return (1);
+		exit(98);
 	}
 
-			num1 = atoi(argv[1]);
-			num2 = atoi(argv[2]);
-			result = multiply(num1, num2);
+	/* multiply each digit of num1 with num2 */
+	for (i = num1_len - 1; i >= 0; i--)
+	{
+		for (j = num2_len - 1; j >= 0; j--)
+		{
+			/* multiply each digit and add to result */
+			int mul = (av[1][i] - '0') * (av[2][j] - '0');
 
-			printf("%u\n", result);
+			/* add to previous result */
+			int sum = result[i + j + 1] + mul;
 
-			return (0);
+			/* update result */
+			result[i + j] += sum / 10;
+			result[i + j + 1] = sum % 10;
+		}
+	}
+
+	/* print result and free the allocated memory */
+	print_result(result, num1_len + num2_len);
+	free(result);
+	return (0);
 }
