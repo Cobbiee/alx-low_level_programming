ca2e9b1fb7ad4853529df47d3cf7a826b6e3420b
Cobbiee <equoby@outlook.com>
2023-07-17 16:00:00 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees memory allocated for a dog structure
+ * @d: Pointer to the dog structure to free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
