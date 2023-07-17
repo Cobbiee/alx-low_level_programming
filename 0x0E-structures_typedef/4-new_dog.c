643b97c6038ee0d163be6dec70d93f39aba6605d
Cobbiee <equoby@outlook.com>
2023-07-17 16:56:27 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,45 +11,48 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    int i, name_len, owner_len;
-    char *new_name, *new_owner;
+	dog_t *new_dog;
+	int i, name_len, owner_len;
+	char *new_name, *new_owner;
 
-    if (name == NULL || owner == NULL)
-        return (NULL);
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
 
-    for (name_len = 0; name[name_len]; name_len++)
-        ;
-    new_name = malloc(name_len + 1);
-    if (new_name == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
-    for (i = 0; i < name_len; i++)
-        new_name[i] = name[i];
-    new_name[i] = '\0';
+	for (name_len = 0; name[name_len]; name_len++)
+		;
+	new_name = malloc(name_len + 1);
+	if (new_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 
-    for (owner_len = 0; owner[owner_len]; owner_len++)
-        ;
-    new_owner = malloc(owner_len + 1);
-    if (new_owner == NULL)
-    {
-        free(new_name);
-        free(new_dog);
-        return (NULL);
-    }
-    for (i = 0; i < owner_len; i++)
-        new_owner[i] = owner[i];
-    new_owner[i] = '\0';
+	for (i = 0; i < name_len; i++)
+		new_name[i] = name[i];
+	new_name[i] = '\0';
 
-    new_dog->name = new_name;
-    new_dog->age = age;
-    new_dog->owner = new_owner;
+	for (owner_len = 0; owner[owner_len]; owner_len++)
+		;
+	new_owner = malloc(owner_len + 1);
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; i < owner_len; i++)
+		new_owner[i] = owner[i];
+	new_owner[i] = '\0';
 
-    return (new_dog);
+	new_dog->name = new_name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
 }
