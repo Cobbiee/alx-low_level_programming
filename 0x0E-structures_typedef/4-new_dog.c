5a4623743720cbf21e7762df132260baac01e4cc
Cobbiee <equoby@outlook.com>
2023-07-17 16:21:45 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,40 +1,55 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * new_dog - Creates a new dog structure
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  *
- * Return: Pointer to the new dog structure, or NULL if it fails
+ * Return: pointer to new dog or NULL if fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
+    dog_t *new_dog;
+    int i, name_len, owner_len;
+    char *new_name, *new_owner;
 
-	if (new_dog == NULL)
-		return (NULL);
+    if (name == NULL || owner == NULL)
+        return (NULL);
 
-	new_dog->name = strdup(name);
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return (NULL);
 
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-			return (NULL);
-	}
+    for (name_len = 0; name[name_len]; name_len++)
+        ;
+    new_name = malloc(name_len + 1);
+    if (new_name == NULL)
+    {
+        free(new_dog);
+        return (NULL);
+    }
+    for (i = 0; i < name_len; i++)
+        new_name[i] = name[i];
+    new_name[i] = '\0';
 
-	new_dog->age = age;
+    for (owner_len = 0; owner[owner_len]; owner_len++)
+        ;
+    new_owner = malloc(owner_len + 1);
+    if (new_owner == NULL)
+    {
+        free(new_name);
+        free(new_dog);
+        return (NULL);
+    }
+    for (i = 0; i < owner_len; i++)
+        new_owner[i] = owner[i];
+    new_owner[i] = '\0';
 
-	new_dog->owner = strdup(owner);
+    new_dog->name = new_name;
+    new_dog->age = age;
+    new_dog->owner = new_owner;
 
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-			return (NULL);
-	}
-
-	return (new_dog);
+    return (new_dog);
 }
