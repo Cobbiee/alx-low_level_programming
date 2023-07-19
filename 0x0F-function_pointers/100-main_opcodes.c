06765daa2db366ccad133dbe48d7b7b98e455f03
Cobbiee <equoby@outlook.com>
2023-07-19 15:59:56 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,34 +2,44 @@
 #include <stdlib.h>
 
 /**
- * main - Prints the opcodes of its own main function
- * @argc: The number of command-line arguments
- * @argv: An array of command-line argument strings
+ * error - prints error message and exits
  *
- * Return: Always 0
+ * @err: error code
+ */
+void error(int err)
+{
+	printf("Error\n");
+	exit(err);
+}
+
+/**
+ * main - prints the opcodes of its own main function.
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-    int i, num_bytes;
+	int i, n;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+	/* check for correct number of arguments */
+	if (argc != 2)
+		error(1);
 
-    num_bytes = atoi(argv[1]);
+	/* get number of bytes to print */
+	n = atoi(argv[1]);
+	if (n < 0)
+		error(2);
 
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
-
-    for (i = 0; i < num_bytes; i++)
-        printf("%02x ", ((unsigned char *)main)[i]);
-
-    printf("\n");
-
-    return 0;
+	/* print opcodes */
+	for (i = 0; i < n; i++)
+	{
+		printf("%02hhx", ((char *)main)[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
