81e6df1771ff9e5cd2c84375428df337f9a0dedc
Cobbiee <equoby@outlook.com>
2023-07-20 08:51:42 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
-#ifndef _VARIADIC_FUNCTION_H
-#define _VARIADIC_FUNCTION_H
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 
-#endif /* _VARIADIC_FUNCTION_H */
+#endif /* _VARIADIC_FUNCTIONS_H */
