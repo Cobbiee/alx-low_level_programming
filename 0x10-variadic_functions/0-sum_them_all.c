8ad9e5b0083152a3dd5a44e452e7ce200160e7af
Cobbiee <equoby@outlook.com>
2023-07-20 08:39:45 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_function.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Returns the sum of all its parameters
+ * @n: Number of parameters
+ *
+ * Return: The sum of all parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int result = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_list args;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		result += va_arg(args, int);
+	
+	va_end(args);
+
+	return (result);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef _VARIADIC_FUNCTION_H
+#define _VARIADIC_FUNCTION_H
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* _VARIADIC_FUNCTION_H */
