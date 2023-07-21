eb8bf9e4d28d9c1ba7110f103da2d69570b25176
Cobbiee <equoby@outlook.com>
2023-07-21 14:58:52 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,11 @@
  * print_name - Print a name using a function pointer
  * @name: THe name to print
  * @f: The function pointer to use
+ *
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-	{
+	if (name && f)
 		f(name);
-	}
 }
