073461e6ea7f08ee69de6fcb38e8ea24b7ee336a
Cobbiee <equoby@outlook.com>
2023-07-27 16:51:58 -0700

    3-add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,6 +14,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node;
 	list_t *last_node;
 	unsigned int len = 0;
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
