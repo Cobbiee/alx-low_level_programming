a7486df8b30825fa975125fefcd0c5dad293413d
Cobbiee <equoby@outlook.com>
2023-07-27 16:50:07 -0700

    3-add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,33 +14,24 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node;
 	list_t *last_node;
 	unsigned int len = 0;
-
-	/* allocate memory for the new node */
-	new_node = maloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-
-	/* duplicate the string and store it in the new node */
 	new_node->str = strdup(str);
-
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
-	/* calculate the length of the string */
 	while (str[len] != '\0')
 	{
 		len++;
 	}
 	new_node->len = len;
 
-	/* make the new node the last of the list */
 	new_node->next = NULL;
-
 	if (*head == NULL)
 	{
 		*head = new_node;
@@ -48,13 +39,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		last_node = *head;
-
 		while (last_node->next != NULL)
 		{
 			last_node = last_node->next;
 		}
 		last_node->next = new_node;
 	}
-
 	return (new_node);
 }
