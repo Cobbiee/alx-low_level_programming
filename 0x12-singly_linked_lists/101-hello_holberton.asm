784d7854a476b5d55d1ce3b0a141f0d04cd2153e
Cobbiee <equoby@outlook.com>
2023-07-28 10:20:57 -0700

    101-hello_holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+; hello.asm
+extern printf	; declare printf from the C library
+global main	; define the entry point
+
+section .text		; code segment
+main:			; entry point
+    sub rsp, 40		; align the stack pointer
+    mov rdi, msg	; first argument: message string
+    xor rax, rax	; clear rax (number of floating point arguments)
+    call printf		; call printf
+    add rsp, 40		; restore the stack pointer
+    ret			; return
+
+section .data		; data segment
+msg: db "Hello, Holberton", 10, 0	; message string with newline and null terminator
