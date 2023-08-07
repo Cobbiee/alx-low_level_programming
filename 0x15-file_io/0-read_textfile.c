2862aa7bbe045d8309a355a8dbc1f9acb650b1a5
Cobbiee <equoby@outlook.com>
2023-08-07 16:15:18 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	nread read(fd, buffer, letters);
+	nread = read(fd, buffer, letters);
 	if (nread == -1)
 	{
 		free(buffer);
