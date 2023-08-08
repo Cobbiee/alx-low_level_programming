190f04ea3074631276fc647ca82c4e4a895f4e26
Cobbiee <equoby@outlook.com>
2023-08-08 00:42:16 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(fiename, O_CREAT | O_WRONGLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONGLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
