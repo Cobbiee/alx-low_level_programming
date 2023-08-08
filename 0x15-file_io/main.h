b188ea7e40b480686a2cf2273c59d9367fee3b6a
Cobbiee <equoby@outlook.com>
2023-08-08 14:42:21 -0700

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *file_from, const char *file_to);
 
 #endif /* MAIN_H */
