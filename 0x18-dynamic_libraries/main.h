5f110f8c78db0f7bc080dc97cac949d8978d18e5
Cobbiee <equoby@outlook.com>
2023-09-21 11:15:44 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*function protypes here*/
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -25,4 +24,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
